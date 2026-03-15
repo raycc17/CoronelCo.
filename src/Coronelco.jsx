8b4e828e3fa19bb91cf3bc517ebf7e12377affd0
raycc17 <raycoroneldelcarpio@gmail.com>
2026-03-15 11:17:18 -0500

    Update Coronelco.jsx

--- src/Coronelco.jsx
+++ src/Coronelco.jsx
@@ -691,12 +691,12 @@ export default function Coronelco() {
         </header>
 
         <section className="hero">
-          <h1>
+          <h2>
             <span className="hero-line">CREAMOS</span>
             <span className="hero-line">POSICIONAMOS</span>
             <span className="hero-line">POTENCIAMOS</span>
             <span className="hero-line-sub">tu marca</span>
-          </h1>
+          </h2>
         </section>
 
         <div className="mid-claim-wrap">
