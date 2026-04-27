e1680793ba88d1409f1d14f3a9d477108661df0f
raycc17 <raycoroneldelcarpio@gmail.com>
2026-04-27 10:55:53 -0500

    Create PromoLanzamiento.jsx

---
+++ src/PromoLanzamiento.jsx
@@ -0,0 +1,8 @@
+export default function PromLanzamiento() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Promoción Lanzamiento</h1>
+      <p>Aquí irá todo tu contenido estratégico.</p>
+    </div>
+  )
+}
