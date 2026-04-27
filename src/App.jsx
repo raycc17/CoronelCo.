04d9810c9d795b43e91e286e7315a3710d1b8c33
raycc17 <raycoroneldelcarpio@gmail.com>
2026-04-27 11:00:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import Coronelco from './Coronelco'
-
+import { Routes, Route } from "react-router-dom"
+import Coronelco from "./Coronelco.jsx"
+import PromLanzamiento from "./PromLanzamiento.jsx"
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Coronelco />} />
+      <Route path="/promlanzamiento" element={<PromLanzamiento />} />
+    </Routes>
+  )
+  
 export default function App() {
   return <Coronelco />
 }
+
+
