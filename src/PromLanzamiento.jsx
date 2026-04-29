e58ff9c695934d0b7dfcf55d55ff01ff81f999b4
raycc17 <raycoroneldelcarpio@gmail.com>
2026-04-29 12:09:14 -0500

    Update PromLanzamiento.jsx

--- src/PromLanzamiento.jsx
+++ src/PromLanzamiento.jsx
@@ -90,7 +90,7 @@ export default function PromoLanzamiento() {
 
           .promo-page {
           justify-content: flex-start; /* en vez de centrar */
-          padding-top: 140px; /* ajusta separación con el logo */
+          padding-top: 100px; /* ajusta separación con el logo */
   }
 
           .promo-hero {
