62da3074ac31a24b95ae89770be428653fc64f09
raycc17 <raycoroneldelcarpio@gmail.com>
2026-05-27 13:03:25 -0500

    Update PromLanzamiento.jsx

--- src/PromLanzamiento.jsx
+++ src/PromLanzamiento.jsx
@@ -319,7 +319,7 @@ transform: translateY(0);
   opacity: 0;
   filter: blur(12px);
 
-  animation-duration: 8s;
+  animation-duration: 4s;
   animation-iteration-count: infinite;
   animation-timing-function: ease-in-out;
 }
