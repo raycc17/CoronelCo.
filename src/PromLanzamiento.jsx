eb49c314cd237f4190c1f1544e5edeafade7b4d6
raycc17 <raycoroneldelcarpio@gmail.com>
2026-05-27 20:31:29 -0500

    Update PromLanzamiento.jsx

--- src/PromLanzamiento.jsx
+++ src/PromLanzamiento.jsx
@@ -220,7 +220,7 @@
   background: linear-gradient(135deg, #4919af, #0f64f3);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  line-height: 6;
+  line-height: 1;
 }
 .text-emergency2 {
   margin-top: -9px;
