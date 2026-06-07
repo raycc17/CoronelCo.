f9156cdcfe78ecd771a9eb22406408925711db47
raycc17 <raycoroneldelcarpio@gmail.com>
2026-06-07 16:48:23 -0500

    Update ComingSoon.jsx

--- src/ComingSoon.jsx
+++ src/ComingSoon.jsx
@@ -161,7 +161,7 @@ export default function ComingSoon() {
       }
 
       .helmet-2{
-        bottom:1%;
+        bottom:0.3%;
         width:850px;
         right:22%;
         transform:rotate(35deg);
