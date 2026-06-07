3652375ad263cd943342d8a11b511f0cf684a0f5
raycc17 <raycoroneldelcarpio@gmail.com>
2026-06-07 18:51:59 -0500

    Update ComingSoon.jsx

--- src/ComingSoon.jsx
+++ src/ComingSoon.jsx
@@ -198,14 +198,14 @@ export default function ComingSoon() {
  
        .helmet-1{
          width:350px;
-         left:0px;
+         left:10px;
          top:3%;
   }
 
       .cone-2{
         width:320px;
-        right:0px;
-        top:55%;
+        right:7px;
+        top:53%;
   }
 
 }
