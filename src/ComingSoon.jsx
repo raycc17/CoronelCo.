f200fd661d82cb0ddbbd76bf60059560fb4a85ae
raycc17 <raycoroneldelcarpio@gmail.com>
2026-06-07 15:08:33 -0500

    Create ComingSoon.jsx

---
+++ src/ComingSoon.jsx
@@ -0,0 +1,25 @@
+export default function ComingSoon() {
+  return (
+    <main className="coming-page">
+
+      <div className="blur-orb"></div>
+
+      <img src="/casco.png" className="helmet helmet-1" />
+      <img src="/casco.png" className="helmet helmet-2" />
+
+      <img src="/cono.png" className="cone cone-1" />
+      <img src="/cono.png" className="cone cone-2" />
+
+      <h1 className="coming-title">
+        ESTAMOS
+        <br />
+        CONSTRUYENDO
+      </h1>
+
+      <p className="coming-subtitle">
+        Volvemos pronto.
+      </p>
+
+    </main>
+  )
+}
