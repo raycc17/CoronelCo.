1aacd8c41b28a210d216580698b30da39f867aee
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-17 12:37:04 -0500

    Update Coronelco.jsx

--- src/Coronelco.jsx
+++ src/Coronelco.jsx
@@ -1731,7 +1731,7 @@ box-shadow: 0 6px 14px rgba(0,0,0,0.15);
 
   position:fixed;
 
-  top:68%;
+  top:78%;
   right:-78px;
 
   transform:translateY(-50%);
@@ -1748,7 +1748,7 @@ box-shadow: 0 6px 14px rgba(0,0,0,0.15);
   justify-content:center;
   align-items:center;
 
-  gap:7px;
+  gap:5px;
 
   padding:18px 8px;
 
